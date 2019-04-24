f604d22b245930c86c88d36e1d7d2e4708e6e9d3
MFAC <ninafroes@gmail.com>
2019-04-24 10:48:36 +0200

    feat: Add player handle input method

--- js/app.js
+++ js/app.js
@@ -34,3 +34,13 @@ const allEnemies = [];
 
 const player = {};
 
+document.addEventListener('keyup', e => {
+  const allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+
+  Player.handleInput(allowedKeys[e.keyCode]);
+})
